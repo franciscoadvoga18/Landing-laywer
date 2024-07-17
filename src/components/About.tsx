0b8fb62dc1ece3021b1414233dc2d3be76f588c3
franciscoadvoga18 <franciscoassisadvoga@gmail.com>
2024-07-17 16:33:26 -0300

    mais mudanças

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,7 +8,8 @@ export const About = () => {
                     <div className="flex items-center justify-center lg:items-start lg:justify-start flex-col">
                         <h2 className="text-lg lg:text-3xl font-normalidad-bold">Sobre mim</h2>
                         <div className="h-1 w-20 bg-red-500 my-8"></div>
-                        <p className="max-w-prose">Olá! Sou Francisco de Assis, advogado com 29 anos, casado e dedicado à prática jurídica desde 2015. Minha paixão pelo Direito começou na universidade, onde me formei em 2019, e desde então, tenho me empenhado em construir uma carreira sólida e diversificada.</p>
+                        <p className="max-w-prose">A constante busca pelo aprimoramento profissional me levou a investir em minha formação contínua, participando ativamente de seminários e palestras, além de concluir pós-graduações em Direito Público, Direito Previdenciário e Direito Civil e outros. Essas experiências reforçam meu compromisso com a excelência no serviço ao cliente e a busca incessante por conhecimento.</p>
+                        <p className="max-w-prose mt-4">Desde então, venho adquirindo experiências em diversas áreas do Direito.Faço parte da Comissão de Educação e Ensino Jurídico e do Direito do Trabalho, o que me permite contribuir ativamente para o desenvolvimento e aprimoramento dessas áreas, além de manter-me atualizado com as mais recentes tendências e práticas jurídicas.</p>
                     </div>
                     <div className="w-full flex items-center justify-center gap-4 lg:justify-end mt-8 lg:mt-0">
                         <div className="bg-white p-4 hidden lg:block">
